9393279c90b69d0012540aacd129d23dcea0bbeb
Johnny <merkel.johnny@gmail.com>
2017-09-17 11:36:00 +0200

    Added large_hero.css. Began working with BAM style.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 gulp.task('default',function(){
@@ -16,7 +17,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([cssvars, autoprefixer, nested]))
+	.pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
 	.pipe(gulp.dest('./app/tmp/styles'));
 });
 
