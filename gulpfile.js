ff3f7789e2419fcf8acc54ee68e5409aaf5bbad5
Johnny <merkel.johnny@gmail.com>
2017-09-17 09:38:46 +0200

    Finished section 4, lesson 11

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested');
+
+
+gulp.task('default',function(){
+	console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function(){
+	console.log("imagine something useful to be done with html");
+});
+
+gulp.task('styles', function(){
+	return gulp.src('./app/assets/styles/styles.css')
+	.pipe(postcss([cssvars, autoprefixer, nested]))
+	.pipe(gulp.dest('./app/tmp/styles'));
+});
+
+gulp.task('watch', function(){
+	watch('./app/index.html', function(){
+		gulp.start('html');
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('styles');
+	});
+});
