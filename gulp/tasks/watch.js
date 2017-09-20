cfb12702c4fd101e654e7b8dda46db2eb77795a7
Johnny <merkel.johnny@gmail.com>
2017-09-20 21:19:29 +0200

    Created sub files for gulp, Fixed font size-> font-size

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function(){
+	browserSync.init({
+		notify: false,
+		server: {
+			baseDir: "app"
+		}
+	});
+	watch('./app/index.html', function(){
+		browserSync.reload();
+	});
+
+	watch('./app/assets/styles/**/*.css', function(){
+		gulp.start('cssInject');
+	});
+});
+
+/*['styles'] is a dependency task, all dependency tasks   */
+gulp.task('cssInject', ['styles'], function(){
+	gulp.src('./app/tmp/styles/styles.css')
+.pipe(browserSync.stream());
+});
