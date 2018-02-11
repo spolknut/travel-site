c86f6140dbd6eb52320bfd55e23f48f77d474884
Johnny <merkel.johnny@gmail.com>
2018-02-11 12:40:42 +0100

    Finished lesson 38.. Accidentaly worked in sprite branch instead of heading

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,14 +5,14 @@ cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 cssImport = require('postcss-import'),
 mixins = require('postcss-mixins');
-
+hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-  .pipe(postcss([cssImport, mixins, cssvars, autoprefixer, nested]))
+  .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
   .on('error', function(errorInfo){
     console.log(errorInfo.toString());
     this.emit('end');
   })
-  .pipe(gulp.dest('./app/tmp/styles'));
+  .pipe(gulp.dest('./app/temp/styles'));
 });
